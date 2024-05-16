6fd70555169662677c906fbb376839caa3da6b44
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-16 16:38:38 +0200

    creato ciclo che rigira la parola

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 let parolaUtente=prompt("scrivi una parola palindroma");
 
+let parolaUtenteRigirata="";
+
+for (i=parolaUtente.length-1; i >= 0; i--){
+
+      parolaUtenteRigirata=parolaUtenteRigirata + parolaUtente[i]
+
+}
+
+
+console.log(parolaUtenteRigirata);
 
 
