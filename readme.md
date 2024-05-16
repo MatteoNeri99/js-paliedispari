372d9f5f4c148b60f1593415310d15af96f27505
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-16 19:49:48 +0200

    aggiunta rischiesta all'utente
    controllato che la scelta sia giusta

--- readme.md
+++ readme.md
@@ -6,3 +6,26 @@ Creare una funzione per capire se la parola inserita è palindroma
 2) mi assicuro che l'utente abbia scritto una parola 
 3) creo un ciclo che rigiri la parola
 4) controllo che la porola sia uguale anche se scritta al contrario
+
+
+
+
+
+
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+
+
+1) chiedo all' utente di scegliere tra pari o dispari
+2) controllo se la risposta del utente sia giusta
+3) chiedo all'utente di inserire un numero da 1 a 5 
+4) controllo se la risposta dell'utente sia giusta
+5) genero un numero random da 1 a 5 per il pc
+6) sommo i numeri
+7) controllo se la somma sia pari o dispari
+8) dichiaro il vincitore
+
+
--- script.js
+++ script.js
@@ -1,39 +1,54 @@
-let parolaUtente=prompt("scrivi una parola palindroma");
+// let parolaUtente=prompt("scrivi una parola palindroma");
 
-let parolaUtenteRigirata="";
-
-
+// let parolaUtenteRigirata="";
 
 
 
 
 
-function palindroma (parola, parolaUno){
 
-    for (i=parola.length-1; i >= 0; i--){
 
-        parolaUno=parolaUno + parola[i]
+// function palindroma (parola, parolaUno){
+
+//     for (i=parola.length-1; i >= 0; i--){
+
+//         parolaUno=parolaUno + parola[i];
         
         
-    }
-     console.log(parolaUno)
+//     }
+//      console.log(parolaUno)
     
-    if(parola === parolaUno){
+//     if(parola === parolaUno){
 
-        let èPalindroma="bravo la parola inserita è palindroma";
+//         let èPalindroma="bravo la parola inserita è palindroma";
 
-        return èPalindroma;
+//         return èPalindroma;
         
 
-    }else if (parola !== parolaUno){
-        let falso="la parola inserita non è palindroma";
+//     }else if (parola !== parolaUno){
+//         let falso="la parola inserita non è palindroma";
 
-        return falso;
-    }
+//         return falso;
+//     }
 
+// }
+
+
+// let controllo=palindroma(parolaUtente, parolaUtenteRigirata)
+
+// console.log(controllo)
+
+
+
+
+
+let sceltaUtente=prompt("scegli tra pari o dispari");
+
+console.log(sceltaUtente)
+
+if (sceltaUtente !== "pari" && sceltaUtente !== "dispari"){
+
+    sceltaUtente="pari";
+
+    console.log(sceltaUtente)
 }
-
-
-let controllo=palindroma(parolaUtente, parolaUtenteRigirata)
-
-console.log(controllo)
